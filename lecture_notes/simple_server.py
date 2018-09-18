76d2d255978cd3d57a5809c9dac410162de9aee7
Helge <rhp@cphbusiness.dk>
2018-09-18 08:19:18 +0200

    Code from project session and for today

--- lecture_notes/simple_server.py
+++ lecture_notes/simple_server.py
@@ -1,3 +1,4 @@
+import who_am_i
 from flask import Flask
 
 
@@ -6,14 +7,17 @@ my_app = Flask(__name__)
 
 @my_app.route('/')
 def hello():
-    return 'Hej!'
-
-
-@my_app.route('/<book_id>/<pages>')
-def read_books(pages):
-    print(pages)
-    return pages
+    my_ip = who_am_i.get_my_ip()
+    return f'Hej from {my_ip}!'
 
 
 if __name__ == '__main__':
-    my_app.run()
+    my_app.run(host='0.0.0.0')
+
+
+# 10.50.137.35
+# 10.50.136.232
+# 10.50.130.46
+# 10.50.137.11
+# 10.50.130.73
+# 10.50.138.89
